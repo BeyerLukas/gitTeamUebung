e9aeac74a75499be3315034ce2cd39c4da0efec2
beyerlukas@dv-schulen.de <BeyerLukas@bsz.edu>
2020-11-30 08:53:39 +0100

    testpush

---
+++ Greetings.java
@@ -0,0 +1,13 @@
+class Greetings
+{
+	String name;
+	Greeter (String name;)
+	{
+		this.name= name;
+	}
+	public String sendGreetings()
+	{
+		return "Hallo "+name+" ,einen schönen Tag für git!");
+	}
+
+}
